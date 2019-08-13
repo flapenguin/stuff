aeb517f15445956ccf3108c396394795bf48c5ac
flapenguin <flapengemail@gmail.com>
2019-08-14 00:49:06 +0300

    jit: complete modr/m & sib

--- jit/jit.c
+++ jit/jit.c
@@ -29,10 +29,12 @@
 //          end of the instruction.
 // r32    — One of the doubleword general-purpose registers: EAX, ECX, EDX, EBX, ESP, EBP, ESI, EDI; or one of
 //          the doubleword registers (R8D - R15D) available when using REX.R in 64-bit mode
+// r64    — One of the quadword general-purpose registers: RAX, RBX, RCX, RDX, RDI, RSI, RBP, RSP, R8–R15.
+//          These are available when using REX.R and 64-bit mode.
 // imm8   — An immediate byte value. The imm8 symbol is a signed number between –128 and +127 inclusive.
 // imm32  — An immediate doubleword value used for instructions whose operand-size attribute is 32 bits.
 // r/m8   — A byte operand that is either the contents of a byte general-purpose register ... or a byte from memory.
-// r/m32  — A doubleword general-purpose register or memory operand used for instructions whose operandsize attribute is 32 bits
+// r/m64  — A quadword general-purpose register or memory operand used for instructions whose operand-size attribute is 64 bits when using REX.W
 
 // TODO: REX
 // Table 2-4. REX Prefix Fields [BITS: 0100WRXB]
@@ -42,10 +44,10 @@
 typedef enum jj_reg jj_reg;
 enum jj_reg {
   /** Passed as a placeholder when register is unused. */
-  jj_r00 = 0b11111111,
+  jj_rNONE = 0b11111111,
 
-  jj__sib = 0x04,
-  jj__sib_no_base = 0b101,
+  /** Passed as a mask for /digit arguments. */
+  jj__rDIGIT = 0b10000000,
 
   jj_rax = 0b000,
   jj_rcx = 0b001,
@@ -59,16 +61,15 @@ enum jj_reg {
 
 typedef enum jj_scale jj_scale;
 enum jj_scale {
-  jj_s0 = 0b11111111,
   jj_s1 = 0b00000000,
   jj_s2 = 0b01000000,
   jj_s4 = 0b10000000,
   jj_s8 = 0b11000000
 };
 
-typedef struct jj_addrmode jj_addrmode;
-struct jj_addrmode {
-  char type; // 'r' - reg, 'e' - effective address, 'i' - immediate value
+typedef struct jj_op jj_op;
+struct jj_op {
+  char type; // 'r' - reg, 'm' - effective memory address, 'i' - immediate value
   union {
     jj_reg reg;
     uint64_t imm;
@@ -76,171 +77,160 @@ struct jj_addrmode {
       jj_reg base;
       jj_reg index;
       jj_scale scale;
-      uint32_t disp;
-    } eff;
+      int32_t disp;
+    } mem;
   };
 };
 
 typedef struct jj_ctx jj_ctx;
 struct jj_ctx {
   uint8_t* ip;
-  uint32_t last_disp;
-  uint32_t last_disp_size;
+  uint8_t* base;
 };
 
-jj_addrmode jj_mkreg(jj_reg reg) {
-  return (jj_addrmode){.type = 'r', .reg = reg};
+jj_op jj_mkreg(jj_reg reg) {
+  return (jj_op){.type = 'r', .reg = reg};
 }
-jj_addrmode jj_mkimm(uint64_t imm) {
-  return (jj_addrmode){.type = 'i', .imm = imm};
+jj_op jj_mkimm(uint64_t imm) {
+  return (jj_op){.type = 'i', .imm = imm};
 }
-jj_addrmode jj_mkeff(jj_reg base, jj_reg index, jj_scale scale, uint32_t disp) {
-  return (jj_addrmode){.type ='e', .eff = {.base = base, .index = index, .scale = scale, .disp = disp}};
+jj_op jj_mkmem(jj_reg base, jj_reg index, jj_scale scale, int32_t disp) {
+  return (jj_op){.type ='m', .mem = {.base = base, .index = index, .scale = scale, .disp = disp}};
 }
 
-void jj__modrmsib(jj_ctx* ctx, jj_reg reg, jj_addrmode src, uint8_t opcode_ext) {
-  if (src.type == 'i' && opcode_ext <= 0b111) {
-    *ctx->ip++ = 0b11000000 | (opcode_ext << 3) | reg;
-    ctx->last_disp = src.imm;
-    ctx->last_disp_size = 4;
+void jj__ib(jj_ctx* ctx, int8_t imm) {
+  *ctx->ip++ = (uint8_t)imm;
+}
+
+void jj__id(jj_ctx* ctx, int32_t imm) {
+  const uint32_t uimm = (uint32_t)imm;
+  *ctx->ip++ = (uimm >>  0) & 0xff;
+  *ctx->ip++ = (uimm >>  8) & 0xff;
+  *ctx->ip++ = (uimm >> 16) & 0xff;
+  *ctx->ip++ = (uimm >> 24) & 0xff;
+}
+
+void jj__modrmsib(jj_ctx* ctx, jj_reg reg, jj_op rm) {
+  if (rm.type == 'i') DIE("rm cannot be immediate");
+
+  reg = reg & ~jj__rDIGIT;
+  if (rm.type == 'r') {
+    *ctx->ip++ = 0b11000000 | (reg << 3) | rm.reg;
     return;
   }
-
-  if (opcode_ext != 0xff) DIE("opcode extensions (/digit) are supproted only in immediate forms");
-
-  if (src.type == 'r') {
-    *ctx->ip++ = 0b11000000 | (reg << 3) | src.reg;
-    ctx->last_disp = 0;
-    ctx->last_disp_size = 0;
-    return;
-  }
-
-  const jj_reg base = src.eff.base;
-  const jj_reg index = src.eff.index;
-  const jj_scale scale = src.eff.scale;
-  const uint32_t disp = src.eff.disp;
-
-  const bool scaled = scale != jj_s0;
-  uint8_t disp_size = scaled ? 4 : disp <= 255 ? 1 : 4;
 
   // 2.1.5 Addressing-Mode Encoding of ModR/M and SIB Bytes
-  if (scaled && base == jj_rsp) DIE("rsp cannot be used as base for effective address");
+  if (rm.mem.scale != jj_s1 && rm.mem.index == jj_rsp) DIE("rsp cannot be used as base for effective address");
 
+  const bool has_sib = rm.mem.index != jj_rNONE || rm.mem.scale != jj_s1 || rm.mem.base == jj_rsp;
+  const uint8_t disp_size = rm.mem.disp == 0 ? 0 : (uint32_t)rm.mem.disp <= 255 ? 1 : 4;
   const uint8_t mod =
+    disp_size == 0 ? 0b00000000 :
     disp_size == 1 ? 0b01000000 :
-    disp == 0      ? 0b00000000 :
                      0b10000000;
 
-  const uint8_t rm = (scaled ? jj__sib : base);
+  *ctx->ip++ = mod | (reg << 3) | (has_sib ? 0b100 : rm.mem.base);
 
-  *ctx->ip++ = mod | (reg << 3) | rm;
-
-  if (scaled) {
-    if (base == jj_r00) {
-      // 2.1.5 Addressing-Mode Encoding of ModR/M and SIB Bytes
-      // The [*] nomenclature means a disp32 with no base if the MOD is 00B. Otherwise, [*] means disp8 or disp32 + [EBP].
-      // 01 [scaled index] + disp8 + [EBP]
-      // 10 [scaled index] + disp32 + [EBP]
-      if (mod != 0b00000000) DIE("crazy addressing modes with extra ebp are not supported");
-      disp_size = 4;
-    } else {
-      if (disp != 0) DIE("displacement is not supported when both index and base are specified");
-      disp_size = 0;
-    }
-
-    *ctx->ip++ = scale | (index << 3) | (base == jj_r00 ? jj__sib_no_base : base);
+  if (has_sib) {
+    *ctx->ip++ = rm.mem.scale
+      | ((rm.mem.index == jj_rNONE ? 0b100 : rm.mem.index) << 3)
+      | (rm.mem.base == jj_rNONE ? 0b101 : rm.mem.base);
   }
 
-  ctx->last_disp = disp;
-  ctx->last_disp_size = disp_size;
-}
-
-void jj__ib(jj_ctx* ctx, uint8_t imm) {
-  *ctx->ip++ = imm;
-}
-
-void jj__id(jj_ctx* ctx, uint32_t imm) {
-  *ctx->ip++ = (imm >>  0) & 0xff;
-  *ctx->ip++ = (imm >>  8) & 0xff;
-  *ctx->ip++ = (imm >> 16) & 0xff;
-  *ctx->ip++ = (imm >> 24) & 0xff;
-}
-
-void jj__disp(jj_ctx* ctx) {
-  const uint32_t disp = ctx->last_disp;
-  if (ctx->last_disp_size == 1) {
-    jj__ib(ctx, disp);
-  } else if (ctx->last_disp_size == 4) {
-    jj__id(ctx, disp);
+  if (disp_size == 1) {
+    jj__ib(ctx, rm.mem.disp);
+  } else if (disp_size == 4) {
+    jj__id(ctx, rm.mem.disp);
   }
 }
 
-void jj_mov(jj_ctx* ctx, jj_addrmode dst, jj_addrmode src) {
-  if (dst.type != 'r') DIE("dst must be register");
-  if (src.type != 'r') DIE("src must be register");
-
-  // 89 /r | MOV r/m32,r32 | Move r32 to r/m32.
-  *ctx->ip++ = 0x89;
-  jj__modrmsib(ctx, src.reg, dst, 0xff);
-  jj__disp(ctx);
-}
-
-void jj_lea(jj_ctx* ctx, jj_addrmode dst, jj_addrmode src) {
-  if (dst.type != 'r') DIE("dst must be register");
-  if (src.type != 'e') DIE("src must be effective address");
-
-  // 8D /r | LEA r32,m | Store effective address for m in register r32
-  *ctx->ip++ = 0x8d;
-  jj__modrmsib(ctx, dst.reg, src, 0xff);
-  jj__disp(ctx);
-}
-
-void jj_xor(jj_ctx* ctx, jj_addrmode dst, jj_addrmode src) {
-  if (dst.type != 'r') DIE("dst must be register");
-
-  // 31 /r | XOR r/m32, r32 | r/m32 XOR r32.
-  *ctx->ip++ = 0x31;
-  jj__modrmsib(ctx, dst.reg, src, 0xff);
-  jj__disp(ctx);
-}
-
-void jj_add(jj_ctx* ctx, jj_addrmode dst, jj_addrmode src) {
-  if (dst.type != 'r' && dst.type != 'e') DIE("dst must be register or effective address");
-  if (src.type != 'i') DIE("src other than immediate is not implemented");
-
-  if (src.imm == 0) return;
-
-  // 81 /0 id | ADD r/m32, imm32 | Add imm32 to r/m32.
-  *ctx->ip++ = 0x81;
-  jj__modrmsib(ctx, dst.reg, src, 0);
-  jj__disp(ctx);
-}
-
-void jj_sub(jj_ctx* ctx, jj_addrmode dst, jj_addrmode src) {
-  if (src.type != 'i') DIE("src other than immediate is not implemented");
-
-  if (src.imm == 0) return;
-
-  if (dst.type == 'r') {
-    // 81 /5 id | SUB r/m32, imm32 | Subtract imm32 from r/m32.
-    *ctx->ip++ = 0x81;
-    jj__modrmsib(ctx, dst.reg, src, 5);
-    jj__disp(ctx);
+void jj_mov(jj_ctx* ctx, jj_op dst, jj_op src) {
+  if ((dst.type == 'r' || dst.type == 'm') && src.type == 'r') {
+    // REX.W + 89 /r | MOV r/m64,r64 | Move r64 to r/m64.
+    *ctx->ip++ = 0x48; // TODO: REX
+    *ctx->ip++ = 0x89;
+    jj__modrmsib(ctx, src.reg, dst);
     return;
   }
 
-  DIE("effective address as dst is not implemented");
+  DIE("bad operands. supported: MOV r/m64,r64");
 }
 
-void jj_push(jj_ctx* ctx, jj_addrmode op) {
+void jj_lea(jj_ctx* ctx, jj_op dst, jj_op src) {
+  if (dst.type == 'r' && src.type == 'm') {
+    // REX.W + 8D /r | LEA r64,m | Store effective address for m in register r64.
+    *ctx->ip++ = 0x48; // TODO: REX
+    *ctx->ip++ = 0x8d;
+    jj__modrmsib(ctx, dst.reg, src);
+    return;
+  }
+
+  DIE("bad operands. supported: LEA r64,m");
+}
+
+void jj_xor(jj_ctx* ctx, jj_op dst, jj_op src) {
+  if ((dst.type == 'r' || dst.type == 'm') && src.type == 'r') {
+    // REX.W + 31 /r | XOR r/m64, r64 | r/m64 XOR r64
+    *ctx->ip++ = 0x48; // TODO: REX
+    *ctx->ip++ = 0x31;
+    jj__modrmsib(ctx, src.reg, dst);
+    return;
+  }
+
+  DIE("bad operands. supported: XOR r/m64, r64");
+}
+
+void jj_add(jj_ctx* ctx, jj_op dst, jj_op src) {
+  if ((dst.type == 'r' || dst.type == 'm') && src.type == 'r') {
+    // REX.W + 01 /r | ADD r/m64, r64 | Add r64 to r/m64.
+    *ctx->ip++ = 0x48; // TODO: REX
+    *ctx->ip++ = 0x01;
+    jj__modrmsib(ctx, src.reg, dst);
+    return;
+  }
+
+  if ((dst.type == 'r' || dst.type == 'm') && src.type == 'i') {
+    // REX.W + 81 /0 id | ADD r/m64, imm32 | Add imm32 sign-extended to 64-bits to r/m64.
+    *ctx->ip++ = 0x48; // TODO: REX
+    *ctx->ip++ = 0x81;
+    jj__modrmsib(ctx, jj__rDIGIT | 0, dst);
+    jj__id(ctx, src.imm);
+    return;
+  }
+
+  DIE("bad operands. ADD r/m64, r64 and ADD r/m64, imm32 are supported");
+}
+
+void jj_sub(jj_ctx* ctx, jj_op dst, jj_op src) {
+  if ((dst.type == 'r' || dst.type == 'm') && src.type == 'r') {
+    // REX.W + 29 /r | SUB r/m64, r64 | Subtract r64 from r/m64.
+    *ctx->ip++ = 0x48; // TODO: REX
+    *ctx->ip++ = 0x29;
+    jj__modrmsib(ctx, src.reg, dst);
+    return;
+  }
+
+  if ((dst.type == 'r' || dst.type == 'm') && src.type == 'i') {
+    // REX.W + 81 /5 id | SUB r/m64, imm32 | Subtract imm32 sign-extended to 64-bits from r/m64
+    *ctx->ip++ = 0x48; // TODO: REX
+    *ctx->ip++ = 0x81;
+    jj__modrmsib(ctx, jj__rDIGIT | 5, dst);
+    jj__id(ctx, src.imm);
+    return;
+  }
+
+  DIE("bad operands. SUB r/m64, r64 and SUB r/m64, imm32 are supported");
+}
+
+void jj_push(jj_ctx* ctx, jj_op op) {
   if (op.type == 'r') {
-    // 50+rd | PUSH r32 | Push r32.
+    // 50+rd | PUSH r64 | Push r64
     *ctx->ip++ = 0x50 + op.reg;
     return;
   }
 
   if (op.type == 'i') {
-    if (op.imm <= 255) {
+    if (op.imm <= 0xff) {
       // 6A ib | PUSH imm8 | Push imm8.
       *ctx->ip++ = 0x6a;
       jj__ib(ctx, op.imm);
@@ -252,17 +242,21 @@ void jj_push(jj_ctx* ctx, jj_addrmode op) {
     return;
   }
 
-  DIE("effective addresses are not implemented");
+  if (op.type == 'm') {
+    // FF /6 | PUSH r/m64 | Push r/m64
+    *ctx->ip++ = 0xff;
+    jj__modrmsib(ctx, jj__rDIGIT | 6, op);
+  }
 }
 
-void jj_pop(jj_ctx* ctx, jj_addrmode op) {
+void jj_pop(jj_ctx* ctx, jj_op op) {
   if (op.type == 'r') {
-    // 58+ rd | POP r32 | Pop top of stack into r32; increment stack pointer.
+    // 58+ rd | POP r64 | Pop top of stack into r64; increment stack pointer.
     *ctx->ip++ = 0x58 + op.reg;
     return;
   }
 
-  DIE("effective addresses are not implemented");
+  DIE("bad operands. POP r64 is supported");
 }
 
 void jj_ret(jj_ctx* ctx) {
@@ -277,15 +271,25 @@ void jj_leave(jj_ctx* ctx) {
 
 void jj_prologue(jj_ctx* ctx, uint32_t size) {
   jj_push(ctx, jj_mkreg(jj_rbp));
-  *ctx->ip++ = 0x48; // TODO: REX
   jj_mov(ctx, jj_mkreg(jj_rbp), jj_mkreg(jj_rsp));
-  *ctx->ip++ = 0x48; // TODO: REX
   jj_sub(ctx, jj_mkreg(jj_rsp), jj_mkimm(size));
 }
 
 void jj_epilogue(jj_ctx* ctx) {
   jj_leave(ctx);
   jj_ret(ctx);
+}
+
+void jj_dump_disas(jj_ctx* ctx) {
+  FILE* out = fopen("./.jj.dump", "wb");
+  fwrite(ctx->base, 1, ctx->ip - ctx->base, out);
+  fclose(out);
+
+  system(
+    "objdump -D -b binary -M intel,x86-64 -m i386 -j .data ./.jj.dump | awk -F'\n' '$0 ~ /<\\.data>/,0';"
+    "rm ./.jj.dump;"
+  );
+  printf("\n");
 }
 
 int main() {
@@ -299,33 +303,23 @@ int main() {
     return 1;
   }
 
-  uint8_t* const fn_p = page;
-  jj_ctx st_ctx = {fn_p, 0, 0}, *ctx = &st_ctx;
+  jj_ctx __ctx = {page, page};
+  jj_ctx* ctx = &__ctx;
 
-  if (0) {
-    // jj_mov(ctx, jj_mkreg(jj_rax), jj_mkreg(jj_rdi)); // ~~ = edi
-    jj_lea(ctx, jj_mkreg(jj_rax), jj_mkeff(jj_rdi, jj_r00, jj_s0, 0x104)); // ~~ +0x104
-    // jj_lea(ctx, jj_mkreg(jj_rax), jj_mkeff(jj_r00, jj_rdi, jj_s4, 0x00));  // ~~ *4
-    // jj_lea(ctx, jj_mkreg(jj_rax), jj_mkeff(jj_rdi, jj_rdi, jj_s4, 0x00));  // ~~ *5
-    jj_ret(ctx);
-  }
+  jj_prologue(ctx, 0x100);
+  jj_sub(ctx, jj_mkmem(jj_rsi, jj_rNONE, jj_s1, 0), jj_mkimm(0x700));
+  jj_xor(ctx, jj_mkreg(jj_rax), jj_mkreg(jj_rax));
+  jj_add(ctx, jj_mkreg(jj_rax), jj_mkreg(jj_rdi));
+  jj_lea(ctx, jj_mkreg(jj_rax), jj_mkmem(jj_rax, jj_rax, jj_s4, 0));
+  jj_lea(ctx, jj_mkreg(jj_rax), jj_mkmem(jj_rax, jj_rax, jj_s1, 0));
+  jj_epilogue(ctx);
 
-  if (1) {
-    jj_prologue(ctx, 0x404);
-    jj_xor(ctx, jj_mkreg(jj_rax), jj_mkreg(jj_rax));
-    // jj_sub(ctx, jj_mkeff(jj_rsi, jj_r00, jj_s0, 0), jj_mkimm(0xf));
-    jj_epilogue(ctx);
-  }
+  jj_dump_disas(ctx);
 
-  FILE* out = fopen("./jitted", "wb");
-  fwrite(fn_p, 1, ctx->ip - page, out);
-  fclose(out);
-  system("objdump -D -b binary -M intel,x86-64 -m i386 ./jitted; rm ./jitted");
-
-  uint64_t (*fn)(uint64_t x, uint64_t* y) = (void*)fn_p;
-  uint64_t y = 0x100;
-  uint64_t ret = fn(1, &y);
-  printf("result = 0x%" PRIx64 " y = 0x%" PRIx64 "\n", ret, y);
+  uint64_t (*fn)(uint64_t x, uint64_t* y) = (void*)ctx->base;
+  uint64_t arg = 0x900;
+  uint64_t ret = fn(100, &arg);
+  printf("ret = %" PRIu64 "\narg = 0x%" PRIx64 "\n", ret, arg);
 
   return 0;
 }
